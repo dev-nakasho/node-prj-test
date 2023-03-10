06d405dc3f8b4c84fdb74229e1511e4cdde940b0
dev-nakasho <shota9988@gmail.com>
2023-03-11 07:13:17 +0900

    define sample function in src/index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-const val = "test value";
+const val1 = "test";
+const val2 = "sample";
+const val3 = "foo";
+const val4 = "bar";
+const val5 = "baz";
 
-console.log({ val });
+function catString(...args) {
+  let resultString = "";
+  for (const item of args) {
+    // is last item or not
+    if (args.indexOf(item) === args.length - 1) {
+      resultString += item;
+    } else {
+      resultString += item + "-";
+    }
+  }
+  return resultString;
+}
+
+console.log({ result: catString(val1, val2, val3, val4, val5) });
